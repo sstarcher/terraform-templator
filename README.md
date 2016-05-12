e128a6af40e3cfbf519288ac687ce0a9dd902af3
Shane Starcher <shanestarcher@gmail.com>
2016-05-12 09:03:18 -0400

    removing templator capability

--- README.md
+++ README.md
@@ -2,12 +2,7 @@ Terraform Module Templator
 ================
 
 ## Goals
-* Support locking on terraform commands
-* Provide reusable templates
-
-
-## Reason
-In our usage of terraform we wanted to be able to re-use modules with multiple common parameters.  We started by having a new module that contained this common values reference another module.  This in terraform is rough due to having to pass all variables via input/output.  We decided on taking the tact of defining module templates that can be re-used between our environments.
+* Support locking on terraform commands via consul
 
 ## Installing from Github
 ```Bash
@@ -21,75 +16,19 @@ pip install --upgrade git+git://github.com/sstarcher/terraform-templator
 * Folder Structure
 ```
 - Root
-- terraform-template.yaml - This file contains the templates that will be leveraged for each environment
 - - modules
 - - - Bunch of modules
 - - prod - production environment configuration goes here
-- - - files.yaml - any file ending in .yaml will be processed
+- - - files.tf - any file ending in .yaml will be processed
+- - - environment_a - sub environment
+- - - - files.tf
+- - - environment_b - sub environment
+- - - - files.tf
 - - test - testing environment configuration goes here
 - - - same as prod
 ```
 
-* File Structure
-** terraform-template.yaml
-```Yaml
-CONFIGURATION:
-    consul: #If this block exists a consul locking client will be initialized it follows https://github.com/cablehead/python-consul/blob/master/consul/base.py
-        host: consul.service
-variable: # This is your list of variables that  can be used
-    var1: var #This is turned into variable "var1" { default: var } in terraform
-    var2: var
-SourceModule:
-    DEFAULTS:
-        default1: bob
-        default2: null
-    MyModuleName:
-        default2: no longer null
-    SecondModule: {}
-    NotUsedModule: {}
-```
-**  files.yaml
-```Yaml
-SourceModule:
-    MyModuleName: {}
-    SecondModule:
-        default2: defined in files.yaml
-```
-
-* The root level key SourceModule must be a module that is defined under ../modules/
-* Any module defined in `files.yaml` will be instanited and written out to the resulting generated-terraform-templator.tf json file
-* All module templates reference in files.yaml must exist in `terraform-template.yaml`
-* Any parameter that is defined as null in `terraform-template.yaml` must be specified in the `files.yaml` or a validation error will occur
-* Any module template not reference in `files.yaml` will be ignored
-* In the above code `MyModuleName` and `SecondModule` are instantiated.  `MyModuleName` has a default value for `default2` in the template, but `SecondModule` does not.  If `SecondModule` does not define the value `default2` in the `files.yaml` a validation error will occur.
-* Resultant merge would be
-```Yaml
-SourceModule:
-    MyModuleName:
-        default1: bob
-        default2: no longer null
-    SecondModule:
-        default1: bob
-        default2: defined in files.yaml
-```
-* Resultant terraform hcl would look like
-```
-module "MyModuleName" {
-    source = "../modules/SourceModule"
-    default1 = "bob"
-    default2 = "no longer null"
-}
-
-module "SecondModule"
-    source = "../modules/SourceModule"
-    default1 = "bob"
-    default2 = "defined in files.yaml"
-}
-
-```
 
 
 ### Example
-* tft plan -e prod - from within the example folder will generate a generated-terraform-templator.tf that will contain 2 seperate json modules for terraform that have been merged from the terraform-template.yaml file
-
-
+* tft plan -a prod -e us-east-1
--- tft/main.py
+++ tft/main.py
@@ -1,41 +1,35 @@
 #!/usr/bin/env python
 import os
 import sys
-import yaml
-import json
 import argparse
 import logging
 from blessings import Terminal
 import invoke
 import tft.locks.consul_locker as consul
-import copy
 
 
 logger = logging.getLogger('TF')
 
 
+def prepare():
+    invoke.run('terraform fmt > /dev/null')
+    invoke.run('terraform get > /dev/null')
+
+
 def apply(account, args):
+    prepare()
     consul.lock(account)
     invoke.run('terraform apply ' + args)
 
 
 def plan(account, args):
+    prepare()
     consul.lock(account)
-    try:
-        invoke.run('terraform plan ' + args)
-    except invoke.exceptions.Failure:
-        invoke.run('terraform get')
-        print 'Running terraform get for you try again.'
-
-
-def show():
-    invoke.run('terraform show')
-
+    invoke.run('terraform plan ' + args)
 
 commands = {
     "apply": apply,
     "plan": plan,
-    "show": show,
 }
 
 
@@ -91,127 +85,6 @@ CONSUL_HTTP_ADDR - If set attempts to use Consul for locking of Terraform''')
     return args, unknownargs
 
 
-def deep_merge(a, b, path=[]):
-    "merges b into a"
-    if a is None:
-        return b
-    if b is None:
-        return a
-
-    for key in b:
-        if key in a:
-            if isinstance(a[key], dict) and isinstance(b[key], dict):
-                deep_merge(a[key], b[key], path + [str(key)])
-            elif a[key] == b[key]:
-                pass  # same leaf value
-            else:
-                a[key] = b[key]
-        else:
-            a[key] = b[key]
-    return a
-
-
-def gather_yaml():
-    for file in os.listdir("."):
-        if file.endswith(".yaml"):
-            with open(file, 'r') as stream:
-                process_yaml(yaml.load(stream), file)
-
-
-def follow_path(data, path):
-    for value in path:
-        if value in data:
-            data = data[value]
-        else:
-            return None
-    return data
-
-
-def merge_path(data, template, path):
-    common_data = {}
-    if len(path) > 1:
-        common_data = follow_path(template, path[:-1])
-
-    template = follow_path(template, path)
-    if template is None:
-        message = 'A value is being used that does not exist '
-        message = message + 'in the template {t.red}' + '.'.join(path)
-        print(message.format(t=Terminal()))
-        sys.exit(1)
-
-    template_value = copy.deepcopy(template)
-    if 'DEFAULTS' in common_data:  # Merge template common into template
-        common_copy = copy.deepcopy(common_data['DEFAULTS'])
-        template_value = deep_merge(common_copy, template_value)
-
-    return deep_merge(template_value, data)
-
-
-def convert_to_tf(data, home):
-    tf = {'module': {}, 'variable': {}}
-    for key, value in data.iteritems():
-        if key == 'variable':
-            for variable_name, default in value.iteritems():
-                tf[key][variable_name] = {'default': default}
-        else:  # Assume module
-            modules = tf['module']
-            if not value or not isinstance(value.itervalues().next(), dict):
-                modules[key] = value
-                modules[key]['source'] = home + '/modules/' + key
-            else:
-                for module_name, module_data in value.iteritems():
-                    modules[module_name] = module_data
-                    modules[module_name]['source'] = home + '/modules/' + key
-
-    return tf
-
-
-def process_yaml(yaml_data, filename):
-    home = os.path.expanduser(os.environ['TERRAFORM_HOME'])
-    with open(home + "/terraform-template.yaml", 'r') as stream:
-        template = yaml.load(stream)
-        if 'CONFIGURATION' in template:
-            for key, value in template.pop('CONFIGURATION').iteritems():
-                globals()[key].init(value)
-
-        for key, value in yaml_data.iteritems():
-            if not value or not isinstance(value.itervalues().next(), dict):
-                yaml_data[key] = merge_path(value, template, [key])
-            else:
-                common_data = None
-                if 'DEFAULTS' in value:
-                    common_data = value.pop('DEFAULTS')
-                for key2, value2 in value.iteritems():
-                    if common_data:
-                        common_copy = copy.deepcopy(common_data)
-                        value2 = deep_merge(common_copy, value2)
-                    yaml_data[key][key2] = merge_path(value2,
-                                                      template, [key, key2])
-
-    validate(yaml_data)
-    tf_data = convert_to_tf(yaml_data, home)
-    dump(tf_data, filename)
-
-
-def dump(data, filename):
-    with open('{}-generated.tf'.format(filename), 'w') as fp:
-        json.dump(data, fp)
-
-
-def validate(yaml_data, path=[]):
-    errors = []
-    for key, value in yaml_data.iteritems():
-        if value is None:
-            message = 'Validation failed for {t.red}'
-            message = message + '.'.join(path) + '.' + key + '{t.normal}'
-            errors.append(message)
-        elif isinstance(value, dict):
-            validate(value, path + [str(key)])
-    if errors:
-        print('\n'.join(errors).format(t=Terminal()))
-        sys.exit(1)
-
-
 def main():
     if 'TERRAFORM_HOME' in os.environ:
         os.chdir(os.path.expanduser(os.environ['TERRAFORM_HOME']))
@@ -228,7 +101,6 @@ def main():
     if args.environment:
         os.chdir(args.environment)
 
-    gather_yaml()
     commands.get(args.command)(args.account, " ".join(unknown_args))
 
 
